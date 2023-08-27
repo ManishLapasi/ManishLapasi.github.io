4ea428b8cd7d8a8183c9253a0bbeeb22910dab30
mlp7 <mlp7@gatech.edu>
2023-08-26 20:00:10 -0400

    logo on navbar for phone screens

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import './Navbar.css'
 
 let light = '<svg viewBox="0 0 24 24"><circle cx="11.9998" cy="11.9998" r="5.75375" fill="currentColor" transform-origin="11.999799728393555px 11.999799728393555px" style="transform: none; transform-origin: 11.9998px 11.9998px;"></circle><g transform-origin="11.99996030330658px 11.999765396118164px" style="transform: none; transform-origin: 12px 11.9998px;"><circle cx="3.08982" cy="6.85502" r="1.71143" transform="rotate(-60 3.08982 6.85502)" fill="currentColor"></circle><circle cx="3.0903" cy="17.1436" r="1.71143" transform="rotate(-120 3.0903 17.1436)" fill="currentColor"></circle><circle cx="12" cy="22.2881" r="1.71143" fill="currentColor"></circle><circle cx="20.9101" cy="17.1436" r="1.71143" transform="rotate(-60 20.9101 17.1436)" fill="currentColor"></circle><circle cx="20.9101" cy="6.8555" r="1.71143" transform="rotate(-120 20.9101 6.8555)" fill="currentColor"></circle><circle cx="12" cy="1.71143" r="1.71143" fill="currentColor"></circle></g></svg>'
@@ -56,10 +56,47 @@ const handleClick = (setColor) => {
 }
 
 function Navbar({setColor}) {
+
+    const [homeLogo, setHomeLogo] = useState(window.innerWidth>500 ? "text": "logo");
+
+    useEffect(() => {
+      function handleResize() {
+        if(window.innerWidth>500){
+          if(homeLogo!=='text'){setHomeLogo('text')}
+        }
+        else{
+          if(homeLogo!=='logo'){setHomeLogo('logo')}
+        }
+      }
+      window.addEventListener("resize", handleResize)
+      return _ => window.removeEventListener('resize', handleResize)
+    })
+
     return (
         <div>
-            <nav className="navbar">
-            <a href='https://manishlapasi.github.io' rel="noopener noreferrer">manishlapasi.github.io</a>
+            <nav className="navbar"> 
+            <div>
+            {homeLogo==='text' ? <a href='https://manishlapasi.github.io' rel="noopener noreferrer">manishlapasi.github.io</a> : <a href='https://manishlapasi.github.io' rel="noopener noreferrer"><svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+            width="12.000000pt" height="12.000000pt" viewBox="0 0 200.000000 200.000000"
+            preserveAspectRatio="xMidYMid meet">
+
+            <g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)"
+            fill="currentColor" stroke="none">
+            <path d="M538 1409 c-469 -375 -471 -377 -413 -415 23 -15 27 -15 51 1 14 9
+            204 158 421 332 217 173 400 312 407 310 6 -2 191 -148 411 -324 221 -175 406
+            -323 413 -327 35 -21 81 28 62 65 -18 33 -872 709 -895 709 -11 -1 -217 -158
+            -457 -351z"/>
+            <path d="M397 1022 c-15 -17 -17 -57 -17 -370 0 -342 1 -353 21 -373 19 -19
+            29 -20 215 -17 160 2 196 6 204 18 5 8 10 92 10 185 0 94 5 188 11 210 23 81
+            111 134 193 115 48 -11 103 -63 116 -110 5 -19 10 -115 10 -212 0 -171 1 -177
+            22 -192 33 -23 373 -23 406 0 l22 15 0 358 c0 334 -1 359 -18 374 -24 22 -45
+            21 -65 -1 -15 -17 -17 -55 -17 -340 l0 -322 -125 0 -125 0 0 148 c0 184 -13
+            234 -77 303 -54 57 -116 84 -193 83 -68 -1 -115 -21 -168 -69 -75 -67 -86
+            -106 -90 -298 l-4 -168 -122 3 -121 3 -5 328 c-4 277 -7 329 -20 337 -23 15
+            -45 12 -63 -8z"/>
+            </g>
+            </svg></a>}
+            </div>
             <a href='#s1'>Work</a>
             <a href='#s2'>Projects</a>
             <a href='#s3'>Timeline</a>
