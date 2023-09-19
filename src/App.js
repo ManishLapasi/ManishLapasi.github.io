c99502ae7358626cbfb06f42594bf4bffe6b82c3
mlp7 <mlp7@gatech.edu>
2023-09-19 14:13:49 -0400

    artwork prototype, attribution given

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LandingPage from "./Components/LP/LandingPage";
 import WorkExp from "./Components/Exp/WorkExp";
 import Projects from "./Components/Projects/Project";
 import Roads from "./Components/News/Road";
@@ -8,9 +9,7 @@ import SkillSection from "./Components/SkillLogo/SkillSection";
 import './App.css'
 
 function App() {
-  const summaryText = "A software engineer who loves building things for the web, and ensuring they are reliably and scalably performant."
-  const abstractText = "I'm currently pursuing my Master's in Computer Science at Georgia Tech, where I'm majoring in ML / AI and Computing Systems."
-
+  
   const [color, setColor] = useState("156,217,249");
 
   var supportsTouch = 'ontouchstart' in window;
@@ -21,54 +20,7 @@ function App() {
       {!supportsTouch && <Canvas themeColor={color}/>}
       <Navbar setColor={setColor}/>
       <div className="App">
-        <div className="pageOne" style={{display: "flex", flexFlow: "row wrap", alignSelf: "center", justifyContent: "space-evenly", rowGap: "10vh", columnGap: "8vw"}}>
-          <div style={{flexBasis: "40%", flexGrow: 0, display: "flex", flexFlow: "column",rowGap: "10vh"}}>
-            <div className='introBox'>
-                Hello there, I'm
-                <div className="coloredText">
-                  <div>
-                    {[..."Lapasi."].map((x) => <div className='nameDiv'>{x}</div>)}
-                  </div>
-                  <div>
-                    {[..."I write Code."].map((x) => <div className='nameDiv'>{x}</div>)}
-                  </div>
-                </div>
-            </div>
-            <div className='summary'>{summaryText}</div>
-            <div className='summary'>{abstractText}</div>
-          </div>
-          <div class="techbars boxed">
-              <p>Backend</p>
-              <div class="skillbar" id="back-end">
-                <div class="skills back-end"></div>
-              </div>
-
-              <p>Frontend</p>
-              <div class="skillbar" id="front-end">
-                <div class="skills front-end"></div>
-              </div>
-
-              <p>Reliability</p>
-              <div class="skillbar" id="sre">
-                <div class="skills sre"></div>
-              </div>
-
-              <p>DevOps</p>
-              <div class="skillbar" id="devops">
-                <div class="skills devops"></div>
-              </div>
-              <div class="socIcons" style={{marginTop: "10%"}}>
-              <span class="fa-stack fa-2x">
-                  <i class="fa fa-circle fa-stack-2x icon-background" id="linkedin-circle"></i>
-                  <a href="http://linkedin.com/in/manikandanlp"  target="_blank" rel="noopener noreferrer" class="fa fa-linkedin fa-stack-1x" aria-hidden="true"> </a>
-              </span>
-              <span class="fa-stack fa-2x">
-                  <i class="fa fa-circle fa-stack-2x icon-background" id="github-circle"></i>
-                  <a href="http://github.com/manishlapasi"  target="_blank" rel="noopener noreferrer" class="fa fa-github fa-stack-1x" aria-hidden> </a>
-              </span>
-            </div>
-          </div>
-        </div>
+        <LandingPage />
         <div className="heading" id="s1">Skills</div>
         <SkillSection />
         <div className="heading" id="s2">Places I've worked at</div>
